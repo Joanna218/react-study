24132a907bf86fed012ec653d40c4d29bf1668b2
joanna <okokis101@gmail.com>
2020-07-21 12:07:41 +0800

    加入bookSystem元件(表單、Grid做在同一個元件)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+// import component
+import BookSystem from "./Component/BookSystem";
+
 function App() {
   return (
     <div className="App">
-      <p>init</p>
+      <BookSystem />
     </div>
   );
 }
---
+++ src/Component/BookSystem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BookSystem extends Component {
+  render() {
+    return (
+      <div>
+        BookSystem
+      </div>
+    );
+  }
+}
+
+export default BookSystem;
