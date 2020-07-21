e2c4087d79d7a2f438fd13b20ef86a3b02be56c3
joanna <okokis101@gmail.com>
2020-07-21 14:47:47 +0800

    加入 書籍類別下拉選單

--- src/Component/BookSystem.js
+++ src/Component/BookSystem.js
@@ -3,9 +3,17 @@ import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
+import Select from 'react-select';
 // bookData
 import { bookData } from '../book-data';
 
+const bookCategoryItems = [
+  { label: "資料庫", value: "database" },
+  { label: "網際網路", value: "internet" },
+  { label: "家庭保健", value: "home" },
+  { label: "應用系統整合", value: "system" },
+  { label: "語言", value: "language" }
+];
 class BookSystem extends Component {
 
   constructor(props) {
@@ -19,6 +27,11 @@ class BookSystem extends Component {
     return (
       <div id="bookForm">
         <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>書籍類別</Form.Label>
+            <Select options={bookCategoryItems}
+              defaultValue={bookCategoryItems[0]} isSearchable={false} />
+          </Form.Group>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>書籍名稱</Form.Label>
             <Form.Control type="text" placeholder="書籍名稱..." />
@@ -35,6 +48,7 @@ class BookSystem extends Component {
         <BootstrapTable data={ this.state.booksData } pagination bordered={ false }>
           <TableHeaderColumn dataField='BookId' isKey>Product ID</TableHeaderColumn>
           <TableHeaderColumn dataField='BookName'>Product Name</TableHeaderColumn>
+
         </BootstrapTable>
 
       </div>
