6fb986309159cfd63fdd19ec7cf3aed0a295b5c9
joanna <okokis101@gmail.com>
2020-07-21 18:20:32 +0800

    add ref

--- src/Component/BookSystem.js
+++ src/Component/BookSystem.js
@@ -15,6 +15,9 @@ const bookCategoryItems = [
   { label: "語言", value: "language" }
 ];
 class BookSystem extends Component {
+  bookCategory = React.createRef();
+  bookName = React.createRef();
+  bookAuthor = React.createRef();
 
   constructor(props) {
     super(props);
@@ -23,22 +26,31 @@ class BookSystem extends Component {
     }
   }
 
+  addBook = e => {
+    e.preventDefault();
+    const value = this.bookName.current.value;
+    const value2 = this.bookAuthor.current.value;
+    console.log(value);
+    console.log(value2);
+    console.log(this.bookCategory.current.value);
+  }
+
   render() {
     return (
       <div id="bookForm">
-        <Form>
+        <Form onSubmit={this.addBook}>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>書籍類別</Form.Label>
             <Select options={bookCategoryItems}
-              defaultValue={bookCategoryItems[0]} isSearchable={false} />
+              defaultValue={bookCategoryItems[0]} isSearchable={false} ref={this.bookCategory} />
           </Form.Group>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>書籍名稱</Form.Label>
-            <Form.Control type="text" placeholder="書籍名稱..." />
+            <Form.Control type="text" placeholder="書籍名稱..." ref={this.bookName} />
           </Form.Group>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>書籍作者</Form.Label>
-            <Form.Control type="text" placeholder="書籍作者..." />
+            <Form.Control type="text" placeholder="書籍作者..." ref={this.bookAuthor}/>
           </Form.Group>
           <Button variant="primary" type="submit">
             新增
@@ -46,9 +58,11 @@ class BookSystem extends Component {
         </Form>
 
         <BootstrapTable data={ this.state.booksData } pagination bordered={ false }>
-          <TableHeaderColumn dataField='BookId' isKey>Product ID</TableHeaderColumn>
-          <TableHeaderColumn dataField='BookName'>Product Name</TableHeaderColumn>
-
+          <TableHeaderColumn dataField='BookId' isKey width={'10%'}>書籍編號</TableHeaderColumn>
+          <TableHeaderColumn dataField='BookName' width={'30%'}>書籍名稱</TableHeaderColumn>
+          <TableHeaderColumn dataField='BookCategory' width={'20%'}>書籍種類</TableHeaderColumn>
+          <TableHeaderColumn dataField='BookAuthor' width={'20%'}>書籍作者</TableHeaderColumn>
+          <TableHeaderColumn dataField='BookBoughtDate' width={'20%'}>購書日期</TableHeaderColumn>
         </BootstrapTable>
 
       </div>
