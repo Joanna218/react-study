f0ff1eb556b81401748f418fa8d3ac6e6ed406d5
joanna <okokis101@gmail.com>
2020-07-24 15:38:22 +0800

    先拿掉書籍類別，類別下拉選單待研究setSate的問題

--- src/Component/BookSystem.js
+++ src/Component/BookSystem.js
@@ -8,11 +8,11 @@ import Select from 'react-select';
 import { bookData } from '../book-data';
 
 const bookCategoryItems = [
-  { label: "資料庫", value: "database" },
-  { label: "網際網路", value: "internet" },
-  { label: "家庭保健", value: "home" },
-  { label: "應用系統整合", value: "system" },
-  { label: "語言", value: "language" }
+  { text: "資料庫", value: "database" },
+  { text: "網際網路", value: "internet" },
+  { text: "家庭保健", value: "home" },
+  { text: "應用系統整合", value: "system" },
+  { text: "語言", value: "language" }
 ];
 class BookSystem extends Component {
   constructor(props) {
@@ -30,7 +30,7 @@ class BookSystem extends Component {
     e.preventDefault();
     this.state.booksData.push({
       "BookId": this.state.maxBookId + 1,
-      "BookCategory": this.state.bookCategory.label,
+      "BookCategory": this.state.bookCategory,
       "BookName": this.state.bookName,
       "BookAuthor": this.state.bookAuthor,
       "BookBoughtDate":"1990-06-18",
@@ -46,37 +46,37 @@ class BookSystem extends Component {
 
   }
 
-  bookCategoryChange = e => {
-    this.setState({
-      bookCategory: e
-    })
-  }
 
-  getInputChange = e => {
-    const target = e.target;
-    const name = target.name;
-
+  getBookChangeData = e => {
+    const name = e.target.name;
+    const value = e.target.value;
     this.setState({
-      [name]: target.value
+      [name]: value
     });
   }
+
+  getDropDownChangeData = e => {
+    const value = e.target.value;
+    const data = bookCategoryItems.find(item => item.value == value);
+    const a = {
+      text: data.text,
+      value: data.value
+    }
+  }
+
 
   render() {
     return (
       <div id="bookForm">
         <Form onSubmit={this.addBook}>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>書籍類別</Form.Label>
-            <Select options={bookCategoryItems}
-              defaultValue={this.state.bookCategory} isSearchable={false} onChange={this.bookCategoryChange}/>
-          </Form.Group>
+
           <Form.Group controlId="formBasicEmail">
             <Form.Label>書籍名稱</Form.Label>
-            <Form.Control type="text" placeholder="書籍名稱..." name="bookName" value={this.state.bookName} onChange={this.getInputChange}/>
+            <Form.Control type="text" placeholder="書籍名稱..." name="bookName" value={this.state.bookName} onChange={this.getBookChangeData}/>
           </Form.Group>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>書籍作者</Form.Label>
-            <Form.Control type="text" placeholder="書籍作者..." name="bookAuthor" value={this.state.bookAuthor} onChange={this.getInputChange}/>
+            <Form.Control type="text" placeholder="書籍作者..." name="bookAuthor" value={this.state.bookAuthor} onChange={this.getBookChangeData}/>
           </Form.Group>
           <Button variant="primary" type="submit">
             新增
@@ -97,3 +97,12 @@ class BookSystem extends Component {
 }
 
 export default BookSystem;
+
+// <Form.Group controlId="formBasicEmail">
+// <Form.Label>書籍類別</Form.Label>
+// <Form.Control name="bookCategory" as="select" value={this.state.bookCategory.value} onChange={this.getDropDownChangeData}>
+//   {bookCategoryItems.map((e, key) => {
+//     return <option key={key} value={e.value}>{e.text}</option>;
+//   })}
+// </Form.Control>
+// </Form.Group>
